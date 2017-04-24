641a266e7e4f16b649b63b3b82e4b5196fc5d622
Eklavya Sharma <ekurgn@gmail.com>
2017-04-25 03:15:07 +0530

    util: Also show function name in my_perror

--- src/util.c
+++ src/util.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "util.h"
 
-void line_perror(int line) {
+void line_perror(int line, const char* func_name) {
 	char s[20];
-	snprintf(s, 21, "l%d", line);
+	snprintf(s, 21, "%s:%d", func_name, line);
 	perror(s);
 };
--- src/util.h
+++ src/util.h
@@ -1,7 +1,7 @@
 #ifndef H_UTIL
 #define H_UTIL
 
-void line_perror(int line);
+void line_perror(int line, const char* func_name);
 
-#define my_perror() line_perror(__LINE__);
+#define my_perror() line_perror(__LINE__, __func__);
 #endif	// H_UTIL
