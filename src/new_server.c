9ca04d4780f995ace94c032b1d2135d59c39838a
Eklavya Sharma <ekurgn@gmail.com>
2017-04-25 03:16:43 +0530

    Accept connections from players

--- src/new_server.c
+++ src/new_server.c
@@ -10,11 +10,14 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "util.h"
 #include "networking.h"
 #include "game_state.h"
 #include "player.h"
+
+#define PLAYERS_PER_GAME 4
 
 const char usage_fmt[] = "usage: %s server_port\n";
 
@@ -22,31 +25,84 @@ FILE * error_log;
 const char* argv0;
 int sockfd_listen;
 
+player_list_t plist;
+
 void cry_usage(){
 	fprintf(error_log, usage_fmt, argv0);
 	exit(2);
 }
+
+void* player_thread_func(void*);
 
 void * listener_thread_func(void * arg) {
 	// listen on listening socket wiht a maximum of 5 connections queued
 	while(listen(sockfd_listen, 5) == 0) {
 		// establish connection with the new player
 		struct sockaddr_storage client_addr;
-		int sockfd_new_client = accept(sockfd_listen, (struct sockaddr *) &client_addr, sizeof(client_addr)); 
-		int sockifp_new_client = fdopen(sockfd_new_client, "r");
-		int sockofp_new_client = fdopen(sockfd_new_client, "w");
-		setvbuf(sockofp_new_client, NULL, _IONBF, BUFSIZ);
-		print_sock_info(sockfd_new_client, error_log);
+		socklen_t client_addr_sizeof = sizeof(client_addr);
+		int sockfd = accept(sockfd_listen, (struct sockaddr *) &client_addr, &client_addr_sizeof);
+		print_sock_info(sockfd, error_log);
 
-		// get the username
-		
-
-		// check if the username already exists
-		
-		// if username exists then reply with taken
-		// else reply with accept
-
+		player_t* player = get_new_player(-1, NULL);
+		player->sockfd = sockfd;
+		player->ifp = fdopen(sockfd, "r");
+		player->ofp = fdopen(sockfd, "w");
+		setvbuf(player->ofp, NULL, _IONBF, BUFSIZ);
+		player_thread_func(player);
 	}
+	return NULL;
+}
+
+void* player_thread_func(void* arg) {
+	int i;
+	player_t* player = (player_t*)arg;
+
+	if(plist.size == MAX_PLAYERS) {
+		fprintf(player->ofp, "reject\n\n");
+		free(player);
+		return NULL;
+	}
+
+	// get the username
+
+	fscanf(player->ifp, "%s", player->username);
+	// TODO: change to fgets to improve security
+
+	for(i=0; i<MAX_PLAYERS; ++i) {
+		if(plist.arr[i] != NULL && strcmp(plist.arr[i]->username, player->username) == 0) {
+			fprintf(player->ofp, "taken\n\n");
+			free(player);
+			return NULL;
+		}
+	}
+
+	player->player_id = add_player_to_list(&plist, player);
+	fprintf(player->ofp, "accept\n%d\n\n", player->player_id);
+	player->status = UNALLOCATED;
+	(plist.unallocated)++;
+
+	if(plist.unallocated % PLAYERS_PER_GAME == 0) {
+		// allocate a game
+	}
+
+	char ch;
+	while(true) {
+		ch = fgetc(player->ifp);
+		if(ch == EOF) {
+			my_perror();
+			break;
+		}
+		switch(ch) {
+			case ' ':
+			case '\n':
+				break;
+			case 'q':
+				// quit
+			default:
+				fprintf(stderr, "%d (%s) pressed (%c)\n", player->player_id, player->username, ch);
+		}
+	}
+	return NULL;
 }
 
 int main(int argc, char ** argv) {
@@ -62,13 +118,8 @@ int main(int argc, char ** argv) {
 
 	// open up a socket and bind port_no to it
 	sockfd_listen = get_server_socket(port_no);
-	// get listening file descriptor
-	// sockifp_listen = fdopen(sockfd_listen, "r");
 
-	pthread_t listener_thread_id;
-	pthread_create(&listener_thread_id, NULL, listener_thread_func, NULL);
-
-
-	// listen to incoming requests
+	// start listening for incoming requests
+	listener_thread_func(NULL);
 	return 0;
 }
